cfe241b3460ea0c572e61b169abc2617b59f7c94
Rita <132366922+RitAmaral@users.noreply.github.com>
2023-06-21 17:00:40 +0100

    03 ex 2

---
+++ 03_Ex2/app/src/main/java/com/example/a03_ex2/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.a03_ex2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.a03_ex2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonOla.setOnClickListener {
+            val nome = binding.editNome.text.toString()
+            val apelido = binding.editApelido.text.toString()
+
+            if (nome.isEmpty()) {
+                Toast.makeText(applicationContext, "nome não inserido", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                Toast.makeText(applicationContext, "Olá ${nome} ${apelido}", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+
+    }
+}
