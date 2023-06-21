ee17d0a62f8266094c2b246ae623df8f2ef55535
Rita <132366922+RitAmaral@users.noreply.github.com>
2023-06-21 16:54:42 +0100

    03 ex 1

---
+++ 03_Ex1/app/src/main/java/com/example/a03_ex1/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.a03_ex1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.a03_ex1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonOla.setOnClickListener {
+            val nome = binding.editNome.text.toString()
+            val apelido = binding.editApelido.text.toString()
+
+            if (nome.isEmpty()) {
+                binding.textResultado.text = "Nome não inserido"
+            }
+            else {
+                binding.textResultado.text = "Olá ${nome} ${apelido}!"
+            }
+
+        }
+
+
+    }
+}
