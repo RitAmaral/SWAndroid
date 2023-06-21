2a5d811018245ddb1441d86df0d9141e6889208b
Rita <132366922+RitAmaral@users.noreply.github.com>
2023-06-21 15:25:17 +0100

    inserir imagens

--- 02_Ex1/app/src/main/java/com/example/a02_ex1/MainActivity.kt
+++ 02_Ex1/app/src/main/java/com/example/a02_ex1/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.a02_ex1.databinding.ActivityMainBinding
 import kotlin.math.roundToInt
+import kotlin.text.Typography.euro
 
 class MainActivity : AppCompatActivity() {
     //Nota: exercício 1 e 2 - Converter Euros para Dólares
@@ -15,8 +16,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.buttonDolar.setOnClickListener{
-            var euro: Double = binding.editNumber.text.toString().toDouble()
-            var resultado: Double = (euro * 1.09188 * 100).roundToInt().toDouble() / 100
+            val euro: Double = binding.editNumber.text.toString().toDouble()
+            //2 formas de arredondamento:
+            //val resultado: Double = (euro * 1.09188 * 100).roundToInt().toDouble() / 100
+            val resultado: Double = String.format("%.2f", euro * 1.09188).toDouble()
             binding.textResultado.text = "Valor em dólares: ${resultado}$"
         }
     }
