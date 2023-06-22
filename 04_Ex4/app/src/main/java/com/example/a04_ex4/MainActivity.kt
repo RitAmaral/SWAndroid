a99d916c5e9dc40e45eb75ee6767abe10d8b0ae3
Rita <132366922+RitAmaral@users.noreply.github.com>
2023-06-22 12:51:36 +0100

    ex 4

--- 04_Ex4/app/src/main/java/com/example/a04_ex4/MainActivity.kt
+++ 04_Ex4/app/src/main/java/com/example/a04_ex4/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.a04_ex4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.a04_ex4.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    //04 - Exercício 4
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonEntrar.setOnClickListener {
+            val user = binding.editUser.text.toString()
+            val pass = binding.editPass.text.toString()
+            if (user == "user" && pass == "password") {
+                Toast.makeText(applicationContext, "Login Válido", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                Toast.makeText(applicationContext, "Login Inválido", Toast.LENGTH_SHORT).show()
+                val user = ""
+                val pass = ""
+            }
+
+        }
     }
 }
