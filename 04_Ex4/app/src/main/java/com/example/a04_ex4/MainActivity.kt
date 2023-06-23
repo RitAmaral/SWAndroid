a2835fce62ec00b5610bf0b7ee24205fb9df0799
Rita <132366922+RitAmaral@users.noreply.github.com>
2023-06-23 10:12:12 +0100

    ex 4 alterações

--- 04_Ex4/app/src/main/java/com/example/a04_ex4/MainActivity.kt
+++ 04_Ex4/app/src/main/java/com/example/a04_ex4/MainActivity.kt
@@ -18,14 +18,25 @@ class MainActivity : AppCompatActivity() {
         binding.buttonEntrar.setOnClickListener {
             val user = binding.editUser.text.toString().trim()
             val pass = binding.editPass.text.toString().trim()
-            if (user == "user" && pass == "password") {
-                Toast.makeText(applicationContext, "Login Válido", Toast.LENGTH_SHORT).show()
+
+            if (user.isEmpty()){
+                Toast.makeText(applicationContext, "Preencha o username antes de entrar", Toast.LENGTH_SHORT).show()
+            }
+            else if (pass.isEmpty()) {
+                Toast.makeText(applicationContext, "Preencha a password antes de entrar", Toast.LENGTH_LONG).show()
             }
             else {
-                Toast.makeText(applicationContext, "Login Inválido", Toast.LENGTH_SHORT).show()
-                binding.editUser.text = null
-                binding.editPass.text = null
+                if (user == "user" && pass == "pass") {
+                    Toast.makeText(applicationContext, "Bem-vindo, ${user}!", Toast.LENGTH_SHORT).show()
+                }
+                else {
+                    Toast.makeText(applicationContext, "Login Inválido", Toast.LENGTH_LONG).show()
+                    binding.editUser.text = null
+                    binding.editPass.text = null
+                    //ou colocar: binding.editUser.setText("")
+                }
             }
+
 
         }
     }
