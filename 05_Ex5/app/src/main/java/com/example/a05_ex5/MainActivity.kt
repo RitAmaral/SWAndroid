009be09da6d9d37bbd729507b25f9a8958d6fe97
Rita <132366922+RitAmaral@users.noreply.github.com>
2023-06-26 10:30:28 +0100

    ex 5

--- 05_Ex5/app/src/main/java/com/example/a05_ex5/MainActivity.kt
+++ 05_Ex5/app/src/main/java/com/example/a05_ex5/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.a05_ex5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.system.Os
+import com.example.a05_ex5.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonHoras.setOnClickListener {
+            val horas: Int = binding.editHoras.text.toString().toInt()
+            val semanas: Int = horas / 168 //1 semana tem 168 horas
+            val dias: Int = (horas % 168) / 24
+            val horasRestantes: Int = horas % 24
+
+            binding.textSemana.text = "${horas} horas são ${semanas} semanas."
+            binding.textDias.text = "${horas} horas são ${dias} dias."
+            binding.textHoras.text = "${horas} horas são ${horasRestantes} horas."
+        }
     }
 }
