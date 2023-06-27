d1bc7c16d7845aa1e259a1b19211a46ed830a99d
Rita <132366922+RitAmaral@users.noreply.github.com>
2023-06-27 09:37:26 +0100

    ex 1

--- 05_Ex1/app/src/main/java/com/example/a05_ex1/SecondActivity.kt
+++ 05_Ex1/app/src/main/java/com/example/a05_ex1/SecondActivity.kt
@@ -1,11 +1,22 @@
 package com.example.a05_ex1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.a05_ex1.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySecondBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonVoltar.setOnClickListener {
+            //startActivity(Intent(this, MainActivity::class.java))
+            finish() //basta colocar finish que volta para a main activity
+        }
     }
 }
