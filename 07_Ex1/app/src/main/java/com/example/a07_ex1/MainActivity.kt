0f42e3b72a60b6ed98bf47f000e0016211aa26c8
Rita <132366922+RitAmaral@users.noreply.github.com>
2023-07-04 10:21:45 +0100

    ex 01 = lista nomes + toast

---
+++ 07_Ex1/app/src/main/java/com/example/a07_ex1/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.a07_ex1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import com.example.a07_ex1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val listaNomes = ArrayList<String>()
+        listaNomes.add("Rita")
+        listaNomes.add("Maria")
+        listaNomes.add("José")
+        listaNomes.add("Rui")
+        listaNomes.add("António")
+
+        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listaNomes)
+        binding.listNomes.adapter = arrayAdapter
+
+        binding.listNomes.setOnItemClickListener { _, _, position, _ ->
+            Toast.makeText(applicationContext, "Olá ${listaNomes[position]}", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
