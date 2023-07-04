2763b58196b4e6707ee383d90b1ea3d41befb60f
Rita <132366922+RitAmaral@users.noreply.github.com>
2023-07-04 12:30:09 +0100

    07 ex 3 com adicionar editar e remover

--- 07_Ex3/app/src/main/java/com/example/a07_ex3/MainActivity.kt
+++ 07_Ex3/app/src/main/java/com/example/a07_ex3/MainActivity.kt
@@ -4,11 +4,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.example.a07_ex3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    var pos = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +33,36 @@ class MainActivity : AppCompatActivity() {
             i.putExtra("morada", listaAluno[position].morada)
             i.putExtra("email", listaAluno[position].email)
             startActivity(i)
+        }
+        binding.buttonAdicionar.setOnClickListener {
+            val nome = binding.editNome.text.toString()
+            val morada = binding.editMorada.text.toString()
+            val email = binding.editEmail.text.toString()
+            listaAluno.add(Aluno(nome, morada, email))
+            arrayAdapter.notifyDataSetChanged()
+        }
+        binding.listaAlunos.setOnItemLongClickListener { _, _, position, _ ->
+            binding.editNome.setText(listaAluno[position].nome)
+            binding.editMorada.setText(listaAluno[position].morada)
+            binding.editEmail.setText(listaAluno[position].email)
+            pos = position
+            true
+        }
+        binding.buttonEditar.setOnClickListener {
+            if (pos >= 0 && pos < listaAluno.size) {
+                listaAluno[pos].nome = binding.editNome.text.toString()
+                listaAluno[pos].morada = binding.editMorada.text.toString()
+                listaAluno[pos].email = binding.editEmail.text.toString()
+                pos = -1
+                arrayAdapter.notifyDataSetChanged()
+            }
+        }
+        binding.buttonRemover.setOnClickListener {
+            if (pos >= 0 && pos < listaAluno.size) {
+                listaAluno.removeAt(pos)
+                pos = -1
+                arrayAdapter.notifyDataSetChanged()
+            }
         }
     }
 }
