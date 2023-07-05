8f6afc876a4ca04bc9f9002a287f3ea42c0e9397
Rita <132366922+RitAmaral@users.noreply.github.com>
2023-07-05 14:36:34 +0100

    7 ex 02 update

--- 07_Ex2/app/src/main/java/com/example/a07_ex2/MainActivity.kt
+++ 07_Ex2/app/src/main/java/com/example/a07_ex2/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val listaNumeros = ArrayList<Int>()
-        listaNumeros.add(2)
+        listaNumeros.add(2) //adicionar números à lista
         listaNumeros.add(5)
         listaNumeros.add(3)
         listaNumeros.add(6)
@@ -25,19 +25,23 @@ class MainActivity : AppCompatActivity() {
         listaNumeros.add(1)
 
         val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listaNumeros)
-        binding.listNumeros.adapter = arrayAdapter
+        binding.listviewNumeros.adapter = arrayAdapter //ligar a list view à lista
 
-        binding.listNumeros.setOnItemClickListener { _, _, position, _ ->
+        binding.listviewNumeros.setOnItemClickListener { _, _, position, _ ->
             Toast.makeText(applicationContext, listaNumeros[position].toString(), Toast.LENGTH_SHORT).show()
             binding.editNumber.setText(listaNumeros[position].toString()) //se tivermos a trabalhar com inteiros, colocar toString() aqui a na linha em cima
             pos = position
-        }
+        } //se clicarmos num item da lista, aparece um toast com o número e esse número é colocado no campo  edit number
 
         binding.buttonAdicionar.setOnClickListener {
-            val numero = binding.editNumber.text.toString().toInt()
-            listaNumeros.add(numero)
-            arrayAdapter.notifyDataSetChanged()
-            binding.editNumber.setText("")
+            val numero = binding.editNumber.text.toString()
+            if (numero.isNotEmpty()) { //se o campo não tiver vazio..
+                listaNumeros.add(numero.toInt()) //adiciona o número que colocamos no campo
+                arrayAdapter.notifyDataSetChanged() //notifica que houve alteração na listview e coloca lá o número
+                binding.editNumber.setText("") //para o campo de edit number voltar a ficar limpo
+            } else {
+                Toast.makeText(applicationContext, "Insira um valor antes de o adicionar", Toast.LENGTH_SHORT).show()
+            }
         }
 
         binding.buttonEditar.setOnClickListener {
@@ -45,6 +49,7 @@ class MainActivity : AppCompatActivity() {
                 listaNumeros[pos] = binding.editNumber.text.toString().toInt()
                 pos = -1
                 arrayAdapter.notifyDataSetChanged()
+                binding.editNumber.setText("")
             }
         }
         binding.buttonRemover.setOnClickListener {
@@ -52,6 +57,7 @@ class MainActivity : AppCompatActivity() {
                 listaNumeros.removeAt(pos)
                 pos = -1
                 arrayAdapter.notifyDataSetChanged()
+                binding.editNumber.setText("")
             }
         }
     }
